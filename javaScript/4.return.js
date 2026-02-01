ebc2a3ca45db6f627eef6fd61f44592568c7023b
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-01 11:52:16 +0530

    function return practice complete

---
+++ javaScript/4.return.js
@@ -0,0 +1,18 @@
+function tenTimes(number) {
+    const double = number * 10;
+    return double;
+}
+
+const result = tenTimes(5);
+console.log(result);
+console.log(`=====================`)
+
+
+
+function cutHalf(number) {
+    const half = number / 2;
+    return half;
+}
+const marks = 98;
+const res = cutHalf(marks);
+console.log(res);
