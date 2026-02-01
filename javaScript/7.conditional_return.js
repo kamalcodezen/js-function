8ca8c24165c581c96831819b7a874617c89fd591
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-01 13:24:51 +0530

    conditional statement practice  in fucntion and value return practice

---
+++ javaScript/7.conditional_return.js
@@ -0,0 +1,44 @@
+
+// even number check
+function evenNumber(num) {
+    if (num % 2 !== 1) {   // evenNumber check alternative syntax
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(evenNumber(10));
+
+console.log(`----------------`);
+
+// even number check
+function numberEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(numberEven(20));
+
+console.log(`---------------`);
+
+// ODD check
+function numberOdd(num) {
+    if (num % 2 === 1) {
+        return true;
+    }
+    return false;
+}
+
+
+console.log(numberOdd(2));
+
+console.log(`----------------`);
+
+
+// ODD check
+function oddNumber(num) {
+    return num % 2 !== 0 ? true : false;
+}
+console.log(oddNumber(5));
