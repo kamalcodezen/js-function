e837fe11fe7cd2a9589afc1e1dbdb0c6de16238a
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-01 11:20:15 +0530

    function call in different way exm number and variable

---
+++ javaScript/3.argument.js
@@ -0,0 +1,21 @@
+
+function number(a) {
+    console.log(a);
+}
+number(20);
+console.log(`-------------------------`);
+const age = 27;
+number(age);
+console.log(`--------------------------`);
+const name = "kamaluddin";
+number(name);
+
+
+
+function difference(a, b) {
+    const check = a - b;
+    console.log(a, b, "difference", check)
+}
+const fatherAge = 50;
+const sonAge = 25;
+difference(fatherAge, sonAge); // function call in variable;
